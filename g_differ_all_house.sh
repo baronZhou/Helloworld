614a32ea742b3e6961d7dde294c89778a70d6c80
zhouhehe <hehe.zhou@beanpodtech.com>
2018-01-10 08:50:09 +0000

    modify g_differ_all_house.sh

--- g_differ_all_house.sh
+++ g_differ_all_house.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
-COMMITOLD="e9f42a37"
-COMMITNEW="a0ae7f7b"
-SORCE_DR=/home/michael/code/6580O/alps-release-o1.mp2-default_2
-PATCH=/home/michael/code/6580O/bkp_doujia_zhouhehe/111111111111/6580_androidO_patch
-
+SORCE_DR=~/gerrit/workshop/src/leagoo_F518
+PATCH=~/gerrit/workshop/src/leagoo_F518/patch
 NEW=$PATCH/new
 OLD=$PATCH/old
 mkdir $NEW $OLD
@@ -19,19 +16,30 @@ echo "cangku: "$cangku
 echo "commitold: "$commitold
 echo "commitnew: "$commitnew
 
-cd $SORCE_DR/$cangku
+echo "11111111111"
+echo "xx:"$SORCE_DR
+echo "xx:"$cangku
+echo "22222222222222222"
+cd $SORCE_DR
+echo "11111111111aaaaaaaaa"
+cd $cangku
+echo "333333333333333"
 touch 1temp.txt
-cp --parent 1temp.txt $NEW
-cp --parent 1temp.txt $OLD
-rm 1temp.txt
-rm $NEW/1temp.txt
-rm $OLD/1temp.txt
+cd $SORCE_DR
+ls $cangku/1temp.txt
+ls $NEW
+cp --parent $cangku/1temp.txt $NEW
+cp --parent $cangku/1temp.txt $OLD
+rm $cangku/1temp.txt
+rm $NEW/$cangku/1temp.txt
+rm $OLD/$cangku/1temp.txt
 #### $NEW/$cangku   $OLD/$cangku
 
 
 ##########################################################
-COMMITOLD=commitold
-COMMITNEW=commitnew
+COMMITOLD=$commitold
+COMMITNEW=$commitnew
+echo "$$$$$$$$$$$$$$$$$$$$$$$$#"$cangku"$$$$$$$$$$$$$$$$$$$$$$$$#"
 echo "commit old="$COMMITOLD" , commit new="$COMMITNEW
 
 cd $SORCE_DR/$cangku
@@ -43,7 +51,7 @@ git reset $COMMITNEW --hard
 for i in `cat $PATCH/temp.txt`
 do
 #echo "xx:"$i
-cp --parents $i $NEW -r
+cp --parents $i $NEW/$cangku -r
 done
 
 #git revert $COMMITNEW
@@ -52,7 +60,7 @@ git reset $COMMITOLD --hard
 for i in `cat $PATCH/temp.txt`
 do
 #echo "xx:"$i
-cp --parents $i $OLD -r
+cp --parents $i $OLD/$cangku -r
 done
 
 rm $PATCH/temp.txt
