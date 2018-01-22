c71b39e3fb123f6e6d859a23b2e7f154c1b324da
zhouhehe <hehe.zhou@beanpodtech.com>
2018-01-22 02:27:58 +0000

    upload clone_code

---
+++ clone_code/clone_code.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+workshop=~/gerrit/workshop
+CURRENT_DIR=`pwd`
+
+
+null="NULL"
+LOG=$CURRENT_DIR/log.txt
+TEMP1=$CURRENT_DIR/temp1.txt
+
+cd $CURRENT_DIR
+cat $CURRENT_DIR/project_file.txt | grep "##" >$TEMP1
+
+for LINE in `cat $TEMP1`
+do
+platform=`echo $LINE | awk -F"##" '{print $1}'`
+module=`echo $LINE | awk -F"##" '{print $2}'`
+reposist=`echo $LINE | awk -F"##" '{print $3}'`
+branch=`echo $LINE | awk -F"##" '{print $4}'`
+echo "11111111111111111111start11111111">>$LOG
+echo "The "$platform":"$module "start..........." >>$LOG
+echo "platform="$platform" , module="$module" , reposist="$reposist" , branch="$branch >>$LOG
+
+path_name=$workshop/src/$platform
+#path_name=~/workspace/github/Helloworld
+echo "path name="$path_name>>$LOG
+if [ -d "$path_name" ]
+then
+
+cd $path_name
+
+if [ "$branch" = "$null" ]
+then
+git clone $reposist
+#echo "branch="$branch"    , "$module" branch==null"
+else
+git clone $reposist -b $branch
+fi
+
+echo "The "$platform":"$module "download completely." >>$LOG
+#echo -e "\033[31mThe "$platform":"$module "download completely.\033[0m"
+
+fi
+done
+#rm $TEMP1
+echo "THE shell end">>$LOG
